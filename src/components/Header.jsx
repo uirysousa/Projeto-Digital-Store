11b167fca1d0e43c1c85cc71b19122afecfb4fad
uirysousa <uiry.sousa@hotmail.com>
2024-07-22 09:18:24 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,37 +10,83 @@ import { NavLink } from 'react-router-dom';
 
 
 
+const Gg = styled.header`
+    display:flex;
+    width:1440px;
+    height:192px; 
+    flex-direction:column;
+`;
+
 const HeaderContainer = styled.header`
     display:flex;
-    width:"1440px";
-    height:"192px";
+    width:1340px;
+    height:129px;
+    margin-top:34px;
+    margin-left:100px;
     justify-content:space-between;
+    align-items:self-start;
+    & .buscar {
+        display:flex;
+        background-color:#474747; 
+        width:559px;
+        height:60px;
+        border-radius:8px;
+        opacity:100%;
+        &::placeholder{
+        color:white;
+        }  
+    }   
     & nav ul{
         display:flex;
     }
-    & Button{
-        background-color:#474747;
+    & .Cadastrar{
+        color:#474747;
         text-decoration:underline;
-        font-size:"16px";
-        width:"114px";
-        height:"44px";
+        font-size:16px;
+        width:114px;
+        height:44px;
         display:flex;
+        align-items:center;
+        border-radius:10px;
+        font-family:Arial, Helvetica, sans-serif;
     } 
+    & .Entrar{
+        background-color:#C92071;
+        text-decoration:none;
+        border-color:#C92071;
+        font-size:16px;
+        width:114px;
+        height:44px;
+        display:flex;
+        border-radius:10px;
+        font-family:Arial, Helvetica, sans-serif;
+    } 
+    
 
 `;
+
 
 const Navegacao = styled.nav`
     & ul {
         display:flex;
-        width:"422px";
-        height:"29px";
+        width:422px;
+        height:29px;
         flex-direction:row;
         justify-content:flex-start;
         align-items:center;
         gap: 20px;
-    }
-    & ul li{
-        display:flex;
+        margin-left:64px;
+        margin-top:-30px;
+        list-style:none;
+        text-decoration: none;
+
+        & a{
+        text-decoration: none;
+        color:#474747;
+        font-size:16px;
+        font-weight: 400;
+        }
+        
     }
 `;
 
@@ -49,26 +95,28 @@ const Navegacao = styled.nav`
 const Header = () => {
 
     return (
-    <>
+    <Gg>
         <HeaderContainer>
             <Logo />
             <form>
-                <label htmlFor="buscar" className='block uppercase font-bold text-sm mb-1'></label>
-                <div className="flex">
-                    <IconField>
-                        <InputText  placeholder="Pesquisar Produto..." />
-                        <InputIcon className="pi pi-search"> </InputIcon>
-                    </IconField>
-                </div>
+             <div>
+                <IconField>
+                    <div style={{ display:'flex' , alignItems:'center'}}>
+                        <InputText className='buscar'  placeholder="Pesquisar Produto..." />
+                        <InputIcon className="pi pi-search" style={{ display:'flex', color:'#474747', opacity:'50%'}}> </InputIcon>
+                    </div>
+                </IconField>
+            </div>
             </form>
-                <Button className='Cadastrar' label="Cadastre-se" type="link"/>
-
-                <Button label="Entrar" type="link"/>
+                
+                <a className='Cadastrar' href="http://">Cadastre-se</a>
+                
+                <Button className='Entrar' label="Entrar" type="link"/>
 
                 <div>
                 <label htmlFor="compras" ></label>
                     <IconField>
-                        <InputIcon className="pi pi-shopping-cart"> </InputIcon> 
+                        <InputIcon className="pi pi-shopping-cart" style={{ color: '#C92071' }}> </InputIcon> 
                     </IconField>  
                 </div>      
         </HeaderContainer>
@@ -82,7 +130,7 @@ const Header = () => {
                         </ul>
                    </nav>
         </Navegacao>    
-     </>               
+     </Gg>               
     );
 }
  
