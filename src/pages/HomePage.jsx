ce1d2966de8150724c08e1cbec21e81fb49d6f90
uirysousa <uiry.sousa@hotmail.com>
2024-08-08 00:16:21 -0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,6 +12,7 @@ const ConteinerPrincipal = styled.header`
 `
 const HomeConteiner = styled.header`
     display:flex;
+    background-color:#F5F5F5;
     width:1440px;
     height:681px;
     flex-direction: column;
